36c15cdcc12c2da7a0bbb86dccb0cb534e409baf
ejoreo <ejorear@gmail.com>
2019-08-02 14:13:29 -0700

    Generate file structureand add Hello controller

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {StarterApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {StarterApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new StarterApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+
+  return app;
+}
